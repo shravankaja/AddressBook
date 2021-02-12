52a747b99dd2e7c436138652c39c2caebd82315e
shravan-kaja <shravan.kaja@gmail.com>
2021-02-12 11:46:58 +0530

    Initlialized: contact variables

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,5 +1,14 @@
 
 public class AddressBook {
+	public static String FIRST_NAME;
+	public static String LAST_NAME;
+	public static String ADDRESS;
+	public static String CITY;
+	public static String STATE;
+	public static double ZIP_CODE;
+	public static double PHONE_NUMBER;
+	public static String EMAIL;
+
 	public static void main(String Args[]) {
 		System.out.println("Weclome to Address Book");
 	}
