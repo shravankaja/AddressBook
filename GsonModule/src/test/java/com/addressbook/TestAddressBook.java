25d661270dd0acc2408c62df5410827043fdc5c5
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-03-26 19:32:36 +0530

    Added: test case to verify i multiple threads working to write records

--- GsonModule/src/test/java/com/addressbook/TestAddressBook.java
+++ GsonModule/src/test/java/com/addressbook/TestAddressBook.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.*;
 import java.sql.*;
+import java.time.*;
 import java.util.*;
 
 public class TestAddressBook {
@@ -41,23 +42,37 @@ public class TestAddressBook {
 
     @Test
     void givenDatabaseWeShouldBeAbleToObtainNumberORecrodsInDatabse() throws IOException, JSONException {
-        Assertions.assertEquals(3, addressBook.display());
+        Assertions.assertEquals(4, addressBook.display());
     }
 
     @Test
     void givenValueToBeEditedForAContactToUpdateDatabase() throws SQLException, AddressBookException {
-        Assertions.assertTrue(addressBook.update("email", "abc@hu.com", "Abby"));
+        Assertions.assertTrue(addressBook.update("email", "thruys@hu.com", "Bill"));
     }
 
     @Test
     void givenDateRangeWeShouldBeAbleToGetNumberORecords() throws SQLException, AddressBookException {
-        Assertions.assertEquals(2, addressBook.getContactsGivenDateStateOrCity("Telanagana"));
+        Assertions.assertEquals(3, addressBook.getContactsGivenDateStateOrCity("Hyderabad"));
     }
 
     @Test
     void givenDataRecordShouldBeUpatedInAllTables() {
         Assertions.assertTrue(addressBook.writeToDatabase("Haryana", "GNagar", "Hyderabad",
-                "Friend", "India", "Bill", "Kaja", "ki",
+                "Friend", "India", "ki", "Kaja", "ki",
                 "2018-01-26", 50006, "SS@gmail.com", 741852));
+    }
+
+    @Test
+    void givenMultipleContactsWeShouldBeAbleToAddThemUsingThreading() {
+        ArrayList<AddressBook> listToAdd = new ArrayList<>(Arrays.asList(new AddressBook("Haryana", "GNagar", "Hyderabad",
+                        "Friend", "India", "Abby", "Kaja", "ki", "2018-01-26", "50006",
+                        "SS@gmail.com", 741852),
+                new AddressBook("Haryana", "GNagar", "Hyderabad",
+                        "Friend", "India", "p", "Kaja", "ki", "2018-01-26", "50006",
+                        "SS@gmail.com", 741852)));
+        Instant start = Instant.now();
+        addressBook.writeMultipleContactsToAddressBook(listToAdd);
+        Instant end = Instant.now();
+        System.out.println("Duration with thread: " + Duration.between(start, end));
     }
 }
